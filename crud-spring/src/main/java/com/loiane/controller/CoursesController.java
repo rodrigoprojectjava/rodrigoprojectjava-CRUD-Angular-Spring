06010fae6c7d992be3bd3ec483a683739f7a0d03
Rodrigo Oliveira <projetosrodrigojava@gmail.com>
2023-11-04 16:46:48 -0300

    Criado a API com registro do primeiro curso - Com erro

---
+++ crud-spring/src/main/java/com/loiane/controller/CoursesController.java
@@ -0,0 +1,32 @@
+package com.loiane.controller;
+
+import java.util.List;
+
+import com.loiane.model.Course;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.loiane.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CoursesController {
+
+
+    private final CourseRepository courseRepository = null;
+    
+
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public List<Course> list(){
+        return courseRepository.findAll();
+
+    }
+        
+}
